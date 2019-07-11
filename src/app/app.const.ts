adef1b51e4336044c1d6d769ccf3945f18aa46cd
Felipe hister franz <40280453+FelipeHFranz@users.noreply.github.com>
2019-07-11 16:41:26 -0300

    Delete app.const.ts

--- src/app/app.const.ts
+++
@@ -1,9 +0,0 @@
-export const API = {
-    //default: 'http://192.168.103.4:8080/quadro-api/api/'
-    default: 'http://agdatabox.md.utfpr.edu.br/apidata/v2/'
-   // default: 'http://192.168.100.222:8080/adb-api-data/'
-}
-
-export function getDefaultURL(resource: string) {
-    return API.default + resource;
-}
