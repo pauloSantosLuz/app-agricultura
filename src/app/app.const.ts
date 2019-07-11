70fda1c006acd4fff54843f9b9c9c4911a42bf7c
Paulo Henrique Luz <paulohenrique.luz@hotmail.com>
2019-07-11 16:16:54 -0300

    alteração link da api

--- src/app/app.const.ts
+++ src/app/app.const.ts
@@ -1,7 +1,5 @@
 export const API = {
-    //default: 'http://192.168.103.4:8080/quadro-api/api/'
-    default: 'http://agdatabox.md.utfpr.edu.br/apidata/v2/'
-   // default: 'http://192.168.100.222:8080/adb-api-data/'
+	//coloque aqui o link da api
 }
 
 export function getDefaultURL(resource: string) {
